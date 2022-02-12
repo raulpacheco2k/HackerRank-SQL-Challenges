497afa25a39116ff315f79b2efa4ea3daa6c59e1
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-02-12 16:02:39 -0300

    Create Employee Names.sql

---
+++ Basic Select/Employee Names.sql
@@ -0,0 +1,8 @@
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+https://www.hackerrank.com/challenges/name-of-employees
+*/
+
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME;
