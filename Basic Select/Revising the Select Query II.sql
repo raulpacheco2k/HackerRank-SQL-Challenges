1f50026a42a70189ff3891219c9c77a565703997
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-02-12 11:06:04 -0300

    Create Revising the Select Query II.sql

---
+++ Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,9 @@
+/*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+https://www.hackerrank.com/challenges/revising-the-select-query-2
+*/
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA'
+  AND POPULATION > 120000
